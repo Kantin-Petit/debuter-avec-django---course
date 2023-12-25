3ff996f88465113c3b40fc89290cc9de0a04f2b0
kantin petit <kantin_petit@etu.u-bourgogne.fr>
2023-12-25 16:23:24 +0100

    chap 2

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def hello(request):
+    return HttpResponse("<h1>Hello world</h1>")
+
+def about_us(request):
+    return HttpResponse("<h1>À propos</h1> <p>Nous adorons merch !</p>")
