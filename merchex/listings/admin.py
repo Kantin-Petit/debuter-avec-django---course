3eaa97485160720715d81cad4807850488a586d2
Kantin-Petit <kantin.dev@gmail.com>
2023-12-26 11:48:44 +0100

    chap 7

--- merchex/listings/admin.py
+++ merchex/listings/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from listings.models import Band
+class BandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'year_formed', 'genre')
+admin.site.register(Band, BandAdmin)
+
+from listings.models import Listing
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('title', 'types', 'year', 'sold')
+admin.site.register(Listing, ListingAdmin)
