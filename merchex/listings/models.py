038d11c4a3b9d1969c8c2b17e137e09d5082daa2
Kantin-Petit <kantin.dev@gmail.com>
2023-12-26 07:59:48 +0100

    chap 6

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -1,7 +1,41 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Band(models.Model):
-    name = models.CharField(max_length=100)
+
+    class Genre(models.TextChoices):
+        HIP_HOP = 'HH'
+        SYNTH_POP = 'SP'
+        ALTERNATIVE_ROCK = 'AR'
+
+    name = models.fields.CharField(max_length=100)
+    genre = models.fields.CharField(
+        choices=Genre.choices,
+        max_length=5
+    )
+    biography = models.fields.CharField(max_length=1000)
+    year_formed = models.fields.IntegerField(
+        validators=[MinValueValidator(1900), MaxValueValidator(2023)]
+    )
+    activre = models.fields.BooleanField(default=True)
+    official_homepage = models.fields.URLField(null=True, blank=True)
 
 class Listing(models.Model):
+
+    class Type(models.TextChoices):
+        RECORDS = 'Records'
+        CLOTHING = 'Clothing'
+        POSTERS = 'Posters'
+        MISCELLANEOUS = 'Miscellaneous'
+
     title = models.CharField(max_length=100)
+    types = models.CharField(
+        choices=Type.choices,
+        max_length=15
+    )
+    description = models.CharField(max_length=1000)
+    year = models.IntegerField(
+        null=True, 
+        validators=[MinValueValidator(1900), MaxValueValidator(2023)]
+    )
+    sold = models.BooleanField(default=False)
