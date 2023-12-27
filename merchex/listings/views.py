2ba1093b97629a736b55084de0d133089f47f9a9
kantin petit <kantin_petit@etu.u-bourgogne.fr>
2023-12-27 18:59:57 +0100

    chap 10

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -4,17 +4,29 @@ from django.shortcuts import render
 from listings.models import Listing
 from listings.models import Band
 
-def hello(request):
+def band_list(request):
     bands = Band.objects.all()
     return render(request, 
-                  'listings/hello.html', 
+                  'listings/band_list.html', 
                   {'bands': bands})
 
-def listing(request):
+def band_detail(request, id):
+    band = Band.objects.get(id=id)
+    return render(request, 
+                  'listings/band_detail.html', 
+                  {'band': band})
+
+def listing_list(request):
     listings = Listing.objects.all()
     return render(request, 
-                  'listings/listing.html', 
+                  'listings/listing_list.html', 
                   {'listings': listings})
+
+def listing_detail(request, id):
+    listing = Listing.objects.get(id=id)
+    return render(request, 
+                  'listings/listing_detail.html', 
+                  {'listing': listing})
 
 def contact(request):
     return render(request, 'listings/contact.html')
