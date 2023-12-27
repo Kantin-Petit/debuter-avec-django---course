475c9f02dd7ce6931d19897a90aeaabc6d39b1a3
Kantin-Petit <kantin.dev@gmail.com>
2023-12-27 18:59:57 +0100

    chap 10

--- merchex/merchex/urls.py
+++ merchex/merchex/urls.py
@@ -4,8 +4,10 @@ from listings import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', views.hello),
-    path('listing/', views.listing),
+    path('bands/', views.band_list, name='band-list'),
+    path('bands/<int:id>/', views.band_detail, name='band-detail'),
+    path('listings/', views.listing_list, name='listing-list'),
+    path('listings/<int:id>/', views.listing_detail, name='listing-detail'),
     path('contact/', views.contact),
     path('about-us/', views.about_us),
 ]
