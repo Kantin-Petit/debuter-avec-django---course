f6e2622ca5bbfd15f6623325d0036fab6eafcd33
kantin petit <kantin_petit@etu.u-bourgogne.fr>
2023-12-28 10:36:40 +0100

    chap 14

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -44,6 +44,15 @@ def band_update(request, id):
                   'listings/band_update.html',
                   {'form': form})
 
+def band_delete(request, id):
+    band = Band.objects.get(id=id)
+    if request.method == 'POST':
+        band.delete()
+        return redirect('band-list')
+    return render(request,
+                  'listings/band_delete.html',
+                  {'band': band})
+
 ## Listing views
 def listing_list(request):
     listings = Listing.objects.all()
@@ -68,6 +77,28 @@ def listing_create(request):
     return render(request,
             'listings/listing_create.html',
             {'form': form})
+
+def listing_update(request, id):
+    listing = Listing.objects.get(id=id)
+    if request.method == 'POST':
+        form = ListingForm(request.POST, instance=listing)
+        if form.is_valid():
+            listing = form.save()
+            return redirect('listing-detail', listing.id)
+    else:
+        form = ListingForm(instance = listing)
+    return render(request,
+                  'listings/listing_update.html',
+                  {'form': form})
+
+def listing_delete(request, id):
+    listing = Listing.objects.get(id=id)
+    if request.method == 'POST':
+        listing.delete()
+        return redirect('listing-list')
+    return render(request,
+                  'listings/listing_delete.html',
+                  {'listing': listing})
 
 ## Contact views
 def contact(request):
